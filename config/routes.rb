f85e94beadd8313be95cee45b3a38b4bc60fae28
ArthurRLA <arthurdemelorezende@gmail.com>
2026-04-01 20:58:59 -0300

    Adicao de um fronend aceitavel + criacao do "hub" de features

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "ships#index"
+  root "home#index"
 
   resources :ships do
     member do
-      get  "step/:step_number", action: :step, as: :step
+      get  "step/:step_number", action: :step,        as: :step
       patch "step/:step_number", action: :update_step, as: :update_step
     end
   end
